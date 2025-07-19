e649a926db9862cda63f631fc904227317200faa
Aaron "Mikhada" Johnson <mikhada@skyfall.tech>
2025-07-18 23:44:22 -0500

    Fixed POST calls

--- src/apipost.rs
+++ src/apipost.rs
@@ -3,19 +3,12 @@ use serde_json::Value;
 use urlencoding::encode;
 use reqwest::header::{HeaderMap, HeaderValue, CONTENT_LENGTH};
 
-pub async fn chat(ctx: &ApiContext, message: &str) -> Result<serde_json::Value, reqwest::Error> {
-    let url = format!(
-        "{}/chat?password={}&message={}",
-        ctx.base_url,
-        encode(&ctx.password),
-        encode(message)
-    );
-
+pub async fn api_post(ctx: &ApiContext, url: &str) -> Result<Value, reqwest::Error> {
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_LENGTH, HeaderValue::from_static("0"));
 
     let res = ctx.client
-        .post(&url)
+        .post(url)
         .headers(headers)
         .body("")
         .send()
@@ -24,33 +17,43 @@ pub async fn chat(ctx: &ApiContext, message: &str) -> Result<serde_json::Value, 
     res.json().await
 }
 
+pub async fn chat(ctx: &ApiContext, message: &str) -> Result<Value, reqwest::Error> {
+    let url = format!(
+        "{}/chat?password={}&message={}",
+        ctx.base_url,
+        encode(&ctx.password),
+        encode(message)
+    );
+    api_post(ctx, &url).await
+}
+
 pub async fn player_kick(ctx: &ApiContext, unique_id: &str) -> Result<Value, reqwest::Error> {
-    let url = format!("{}/player/kick?password={}", ctx.base_url, ctx.password);
-    let res = ctx.client
-        .post(&url)
-        .form(&[("unique_id", unique_id)])
-        .send()
-        .await?;
-    res.json().await
+    let url = format!(
+        "{}/player/kick?password={}&unique_id={}",
+        ctx.base_url,
+        encode(&ctx.password),
+        encode(unique_id)
+    );
+    api_post(ctx, &url).await
 }
 
 pub async fn player_ban(ctx: &ApiContext, unique_id: &str) -> Result<Value, reqwest::Error> {
-    let url = format!("{}/player/ban?password={}", ctx.base_url, ctx.password);
-    let res = ctx.client
-        .post(&url)
-        .form(&[("unique_id", unique_id)])
-        .send()
-        .await?;
-    res.json().await
+    let url = format!(
+        "{}/player/ban?password={}&unique_id={}",
+        ctx.base_url,
+        encode(&ctx.password),
+        encode(unique_id)
+    );
+    api_post(ctx, &url).await
 }
 
 pub async fn player_unban(ctx: &ApiContext, unique_id: &str) -> Result<Value, reqwest::Error> {
-    let url = format!("{}/player/ban?password={}", ctx.base_url, ctx.password);
-    let res = ctx.client
-        .post(&url)
-        .form(&[("unique_id", unique_id)])
-        .send()
-        .await?;
-    res.json().await
+    let url = format!(
+        "{}/player/unban?password={}&unique_id={}",
+        ctx.base_url,
+        encode(&ctx.password),
+        encode(unique_id)
+    );
+    api_post(ctx, &url).await
 }
 
